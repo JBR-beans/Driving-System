6345a8fc61d2a187ad4fb67b0f27ba303d17c288
RustyBeans <107262647+RustyBeans@users.noreply.github.com>
2025-02-15 08:06:23 -0800

    integrated input axis control and more tweaking

--- Car.cs
+++ Car.cs
@@ -13,18 +13,28 @@ public class Car : UdonSharpBehaviour
 	public float force;
 	public float steerAngle;
 	public float maxSteerAngle;
+	public float steerReturnSpeed;
+
 	private bool isAccelerating;
-	private bool isSteering;
+	private bool isSteering = true;
 	public TextMeshProUGUI steeringdebug;
 	public TextMeshProUGUI motordebug;
 	private bool isSeeted;
 
 	[UdonSynced] public float force_sync;
 	[UdonSynced] public float steer_sync;
-    void Start()
+
+	private bool isOwnerVR;
+	private bool DebugMode = true;
+
+
+	void Start()
     {
-        
-    }
+		if (Networking.LocalPlayer.IsUserInVR())
+		{
+			isOwnerVR = true;
+		}
+	}
 	public override void Interact()
 	{
 		Networking.LocalPlayer.UseAttachedStation();
@@ -40,138 +50,178 @@ public class Car : UdonSharpBehaviour
 		isSeeted = false;
 	}
 
-	void Update()
+	void FixedUpdate()
 	{
+
 		if (Networking.LocalPlayer.IsOwner(this.gameObject))
 		{
 			if (isSeeted == true)
 			{
 				// DESKTOP
-
-				// forward, reverse
-				isAccelerating = false;
-				if (Input.GetKey(KeyCode.W))
+				if (isOwnerVR == false && DebugMode == false)
 				{
-					isAccelerating = true;
-					//foreach (WheelCollider wheel in backWheels)
+					//// forward, reverse
+					//isAccelerating = false;
+					//if (Input.GetKey(KeyCode.W))
 					//{
-					//	wheel.motorTorque = force;
+					//	isAccelerating = true;
+					//	//foreach (WheelCollider wheel in backWheels)
+					//	//{
+					//	//	wheel.motorTorque = force;
+					//	//}
+
+					//	// sync
+
+					//	force_sync = force;
 					//}
-
-					// sync
-
-					force_sync = force;
-				}
-				if (Input.GetKey(KeyCode.S))
-				{
-					isAccelerating = true;
-					//foreach (WheelCollider wheel in backWheels)
+					//if (Input.GetKey(KeyCode.S))
 					//{
-					//	wheel.motorTorque = -force;
+					//	isAccelerating = true;
+					//	//foreach (WheelCollider wheel in backWheels)
+					//	//{
+					//	//	wheel.motorTorque = -force;
+					//	//}
+
+					//	// sync
+
+					//	force_sync = -force;
 					//}
-
-					// sync
-
-					force_sync = -force;
-				}
-				if (isAccelerating == false)
-				{
-					//foreach (WheelCollider wheel in backWheels)
+					//if (isAccelerating == false)
 					//{
-					//	wheel.motorTorque -= force;
-					//	if (wheel.motorTorque < 2)
+					//	//foreach (WheelCollider wheel in backWheels)
+					//	//{
+					//	//	wheel.motorTorque -= force;
+					//	//	if (wheel.motorTorque < 2)
+					//	//	{
+					//	//		wheel.motorTorque = 0;
+					//	//	}
+					//	//}
+
+					//	// sync
+					//	force_sync -= force;
+					//	if (force_sync < 2)
 					//	{
-					//		wheel.motorTorque = 0;
+					//		force_sync = 0;
+					//	}
+
+					//}
+
+					//// steer
+					//isSteering = false;
+					//if (Input.GetKey(KeyCode.A))
+					//{
+					//	isSteering = true;
+					//	//foreach (WheelCollider wheel in frontWheels)
+					//	//{
+					//	//	wheel.steerAngle += -steerAngle;
+					//	//	if (wheel.steerAngle < -maxSteerAngle)
+					//	//	{
+					//	//		wheel.steerAngle = -maxSteerAngle;
+					//	//	}
+					//	//}
+
+					//	// sync
+
+					//	steer_sync += -steerAngle;
+					//	if (steer_sync < -maxSteerAngle)
+					//	{
+					//		steer_sync = -maxSteerAngle;
 					//	}
 					//}
+					//if (Input.GetKey(KeyCode.D))
+					//{
+					//	isSteering = true;
+					//	//foreach (WheelCollider wheel in frontWheels)
+					//	//{
+					//	//	wheel.steerAngle += steerAngle;
+					//	//	if (wheel.steerAngle > maxSteerAngle)
+					//	//	{
+					//	//		wheel.steerAngle = maxSteerAngle;
+					//	//	}
+					//	//}
 
-					// sync
-					force_sync -= force;
-					if (force_sync < 2)
-					{
-						force_sync = 0;
+					//	// sync
+
+					//	steer_sync += steerAngle;
+					//	if (steer_sync > maxSteerAngle)
+					//	{
+					//		steer_sync = maxSteerAngle;
+					//	}
 					}
+					if (isSteering == false)
+					{
+						//foreach (WheelCollider wheel in frontWheels)
+						//{
+						//	if (wheel.steerAngle > 0 && wheel.steerAngle <= maxSteerAngle)
+						//	{
+						//		wheel.steerAngle -= steerAngle;
+						//	}
+						//	if (wheel.steerAngle >= -2 && wheel.steerAngle <= 2)
+						//	{
+						//		wheel.steerAngle = 0;
+						//	}
+						//	if (wheel.steerAngle < 0 && wheel.steerAngle >= -maxSteerAngle)
+						//	{
+						//		wheel.steerAngle += steerAngle;
+						//	}
+						//}
 
+						// sync
+
+						if (steer_sync > 0 && steer_sync <= maxSteerAngle)
+						{
+							steer_sync -= steerAngle;
+						}
+						if (steer_sync >= -2 && steer_sync <= 2)
+						{
+							steer_sync = 0;
+						}
+						if (steer_sync < 0 && steer_sync >= -maxSteerAngle)
+						{
+							steer_sync += steerAngle;
+						}
+					}
 				}
-
-
-
-				// steer
-				isSteering = false;
-
-				if (Input.GetKey(KeyCode.A))
+				if (DebugMode == true)
 				{
-					isSteering = true;
-					//foreach (WheelCollider wheel in frontWheels)
-					//{
-					//	wheel.steerAngle += -steerAngle;
-					//	if (wheel.steerAngle < -maxSteerAngle)
-					//	{
-					//		wheel.steerAngle = -maxSteerAngle;
-					//	}
-					//}
-
-					// sync
-
-					steer_sync += -steerAngle;
+					force_sync = Input.GetAxis("Vertical") * force;
+					
+					steer_sync += Input.GetAxis("Horizontal");
 					if (steer_sync < -maxSteerAngle)
 					{
 						steer_sync = -maxSteerAngle;
 					}
-				}
-				if (Input.GetKey(KeyCode.D))
-				{
-					isSteering = true;
-					//foreach (WheelCollider wheel in frontWheels)
-					//{
-					//	wheel.steerAngle += steerAngle;
-					//	if (wheel.steerAngle > maxSteerAngle)
-					//	{
-					//		wheel.steerAngle = maxSteerAngle;
-					//	}
-					//}
-
-					// sync
-
-					steer_sync += steerAngle;
 					if (steer_sync > maxSteerAngle)
 					{
 						steer_sync = maxSteerAngle;
 					}
-				}
-				if (isSteering == false)
-				{
-					//foreach (WheelCollider wheel in frontWheels)
-					//{
-					//	if (wheel.steerAngle > 0 && wheel.steerAngle <= maxSteerAngle)
-					//	{
-					//		wheel.steerAngle -= steerAngle;
-					//	}
-					//	if (wheel.steerAngle >= -2 && wheel.steerAngle <= 2)
-					//	{
-					//		wheel.steerAngle = 0;
-					//	}
-					//	if (wheel.steerAngle < 0 && wheel.steerAngle >= -maxSteerAngle)
-					//	{
-					//		wheel.steerAngle += steerAngle;
-					//	}
-					//}
+					if (steer_sync < 0)
+					{
+						steer_sync += Time.deltaTime * steerReturnSpeed;
+					}
+					if (steer_sync > 0)
+					{
+						steer_sync += -Time.deltaTime * steerReturnSpeed;
+					}
 
-					// sync
-
-					if (steer_sync > 0 && steer_sync <= maxSteerAngle)
+					if (isSteering == false)
 					{
-						steer_sync -= steerAngle;
+						if (steer_sync > 0 && steer_sync <= maxSteerAngle)
+						{
+							steer_sync += Input.GetAxis("Horizontal");
+						}
+						if (steer_sync >= -2 && steer_sync <= 2)
+						{
+							steer_sync = 0;
+						}
+						if (steer_sync < 0 && steer_sync >= -maxSteerAngle)
+						{
+							steer_sync += Input.GetAxis("Horizontal");
+						}
 					}
-					if (steer_sync >= -2 && steer_sync <= 2)
-					{
-						steer_sync = 0;
-					}
-					if (steer_sync < 0 && steer_sync >= -maxSteerAngle)
-					{
-						steer_sync += steerAngle;
-					}
+					Debug.Log("amongus");
 				}
+
 			}
 		}
 		UpdateCar();
